55ffb82c2519a41b3d5d4a28782ab410e242b175
Agus Adi <gusadi872@gmail.com>
2022-04-27 11:36:42 +0700

    [iOS-C1] add view model for home

--- Shared/View/Home/View/HomeView.swift
+++ Shared/View/Home/View/HomeView.swift
@@ -9,31 +9,33 @@ import SwiftUI
 import CoreData
 
 struct HomeView: View {
-	
+
+	@ObservedObject var viewModel = HomeViewModel()
+
 	var body: some View {
 		List {
-			
+
 		}
 		.navigationTitle(LocalizableText.homeScreenTitle)
 		.toolbar {
 			ToolbarItem(placement: .navigationBarLeading) {
 				Button {
-					
+
 				} label: {
 					Text(LocalizableText.homeScreenGroupText)
 						.foregroundColor(.teal)
 				}
 			}
-			
+
 			ToolbarItem(placement: .navigationBarTrailing) {
 				Button {
-					
+
 				} label: {
 					Image(systemName: "plus")
 						.foregroundColor(.teal)
 				}
 			}
-			
+
 		}
 	}
 }
---
+++ Shared/View/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Contact
+//
+//  Created by Gus Adi on 27/04/22.
+//
+
+import Foundation
+
+final class HomeViewModel: ObservableObject {
+	
+}
