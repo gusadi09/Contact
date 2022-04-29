a1a228ca5285769586fe95c651abaac143764fdc
Agus Adi <gusadi872@gmail.com>
2022-04-29 19:59:54 +0700

    [iOS-C4] fix observed object

--- Shared/Feature/EditContact/View/EditContactView.swift
+++ Shared/Feature/EditContact/View/EditContactView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct EditContactView: View {
 	
-	@ObservableObject var viewModel = EditContactViewModel()
-	
+	@ObservedObject var viewModel = EditContactViewModel()
+
 	var body: some View {
 		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 	}
